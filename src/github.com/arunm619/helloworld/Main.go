f4724be474dda9f22dd4f535259beb5075540b5e
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 12:21:53 +0530

    TypeCasting

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -2,23 +2,11 @@ package main
 
 import "fmt"
 
-//global variable needs to be mentioned with type.
-var global float32 = 32
-
-var (
-	name    string = "Arun"
-	age     int    = 22
-	worksAt string = "Freshworks"
-)
-
 func main() {
-	//local scope takes preference. Name Shadowing.
-	var name = "UknowWho"
-	//j declared but not used error
-	//j:=10
-	fmt.Printf("%v, %T\n", global, global)
-	fmt.Printf("%v, %T\n", name, name)
-	fmt.Printf("%v, %T\n", age, age)
-	fmt.Printf("%v, %T\n", worksAt, worksAt)
+	var i int = 4
+	fmt.Printf("%v, %T\n", i, i)
+	var j float32 = 3.14
+	j = float32(i)
+	fmt.Printf("%v, %T\n", j, j)
 
 }
