f0beb9c7e21d768de8085043f8b4976c593b186f
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 12:27:36 +0530

    String conversion

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var i int = 4
+	var i int = 42
 	fmt.Printf("%v, %T\n", i, i)
 	var j float32 = 3.14
 	j = float32(i)
 	fmt.Printf("%v, %T\n", j, j)
+
+	var s string = "Arun"
+	fmt.Printf("%v, %T\n", s, s)
+
+	s = string(i) //string is just array of bytes.
+	//converting 42 to string is ascii conversion which is *
+	fmt.Printf("%v, %T\n", s, s)
+
+	s = strconv.Itoa(i) //actual itoa method integer to string
+	fmt.Printf("%v, %T\n", s, s)
 
 }
