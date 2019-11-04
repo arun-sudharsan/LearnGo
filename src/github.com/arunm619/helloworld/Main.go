3c043e79c49b10e63cc123fa105ce13567a97614
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 12:00:39 +0530

    Multiple variable definitions

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -2,18 +2,19 @@ package main
 
 import "fmt"
 
-func callME() {
-	fmt.Println("Hello world!")
-}
+//global variable needs to be mentioned with type.
+var global float32 = 32
+
+var (
+	name    string = "Arun"
+	age     int    = 22
+	worksAt string = "Freshworks"
+)
 
 func main() {
-	var i int //declare but no init at this time
-	i = 1     //initialisation happens after some time.
-	fmt.Printf("%v %T\n", i, i)
+	fmt.Printf("%v, %T\n", global, global)
+	fmt.Printf("%v, %T\n", name, name)
+	fmt.Printf("%v, %T\n", age, age)
+	fmt.Printf("%v, %T\n", worksAt, worksAt)
 
-	var j = 10. //start with some value
-	fmt.Printf("%v %T\n", j, j)
-	//either int or float64
-	k := 100.
-	fmt.Printf("%v %T\n", k, k)
 }
