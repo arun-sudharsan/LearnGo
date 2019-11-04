0c329a1d3c01778c3da36a0dd9edacdc9836370f
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 12:47:09 +0530

    Types of Int

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -4,12 +4,31 @@ import "fmt"
 
 func main() {
 
-	var isLearning bool = true
-	fmt.Printf("%v, %T\n", isLearning, isLearning)
-	//false
-	var whatIsDefaultBoolean bool
-	fmt.Printf("%v, %T\n", whatIsDefaultBoolean, whatIsDefaultBoolean)
-	isSame := 1 == 1
-	fmt.Printf("%v, %T\n", isSame, isSame)
+	var normalIntUnspecified int = 42 // unspecified represents platform dependent
+	fmt.Printf("%v, %T\n", normalIntUnspecified, normalIntUnspecified)
 
+	var smallInt int8 = 42 // 8 bit
+	fmt.Printf("%v, %T\n", smallInt, smallInt)
+
+	var mediumInt int16 = 42 // 16bit
+	fmt.Printf("%v, %T\n", mediumInt, mediumInt)
+
+	var regular int32 = 42 // 32 bit
+	fmt.Printf("%v, %T\n", regular, regular)
+
+	//bigger than this? go for big package from math library
+
+	// we also have unsigned ints. add prefix u.
+
+	var UnormalIntUnspecified uint = 42 // unspecified represents platform dependent
+	fmt.Printf("%v, %T\n", UnormalIntUnspecified, UnormalIntUnspecified)
+
+	var UsmallInt uint8 = 42 // 8 bit
+	fmt.Printf("%v, %T\n", UsmallInt, UsmallInt)
+
+	var UmediumInt uint16 = 42 // 16bit
+	fmt.Printf("%v, %T\n", UmediumInt, UmediumInt)
+
+	var Uregular uint32 = 42 // 32 bit
+	fmt.Printf("%v, %T\n", Uregular, Uregular)
 }
