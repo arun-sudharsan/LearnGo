60d8cdca590fca10018565084dd856f78467fa67
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 12:39:22 +0530

    boolean

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -1,25 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	var i int = 42
-	fmt.Printf("%v, %T\n", i, i)
-	var j float32 = 3.14
-	j = float32(i)
-	fmt.Printf("%v, %T\n", j, j)
 
-	var s string = "Arun"
-	fmt.Printf("%v, %T\n", s, s)
-
-	s = string(i) //string is just array of bytes.
-	//converting 42 to string is ascii conversion which is *
-	fmt.Printf("%v, %T\n", s, s)
-
-	s = strconv.Itoa(i) //actual itoa method integer to string
-	fmt.Printf("%v, %T\n", s, s)
+	var isLearning bool = true
+	fmt.Printf("%v, %T\n", isLearning, isLearning)
+	//false
+	var whatIsDefaultBoolean bool
+	fmt.Printf("%v, %T\n", whatIsDefaultBoolean, whatIsDefaultBoolean)
+	isSame := 1 == 1
+	fmt.Printf("%v, %T\n", isSame, isSame)
 
 }
