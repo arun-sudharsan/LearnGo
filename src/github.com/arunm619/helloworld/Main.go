7efddf664bf1f6d095ae9df8205d619941cfe410
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 14:37:57 +0530

    Added strings:

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -4,25 +4,28 @@ import "fmt"
 
 func main() {
 
-	a := 10.4
-	b := 3.2
-
-	fmt.Printf("%v, %T\n", a, a)
-	fmt.Printf("%v, %T\n", b, b)
-
-	fmt.Printf("%v\n", a+b)
-	fmt.Printf("%v\n", a-b)
-	fmt.Printf("%v\n", a*b)
-	fmt.Printf("%v\n", a/b)
-	// % not defined for float
-	//fmt.Printf("%v\n", a%b)
-
-	var n complex64 = 1 + 2i
-	fmt.Printf("%v , %T\n", n, n)
-	fmt.Printf("%v , %T\n", real(n), real(n))
-	fmt.Printf("%v , %T\n", imag(n), imag(n))
-
 	var s string = "This is a string"
 	fmt.Printf("%v , %T\n", s, s)
+
+	//prints 104 because string is array of bytes.
+	fmt.Printf("%v , %T\n", s[1], s[1])
+
+	//convert to string...
+	fmt.Printf("%v , %T\n", string(s[1]), string(s[1]))
+
+	//s[2] = 100
+	//s[2] = 'a'
+	//s[2] = "nothing can go here as it is immutable"
+	fmt.Printf("%v , %T\n", s, s)
+
+	var s2 = "Another String"
+
+	fmt.Printf("%v , %T\n", s+s2, s+s2)
+
+	b := []byte(s)
+	fmt.Printf("%v , %T\n", b, b)
+
+	c := 'a'
+	fmt.Printf("%v , %T\n", c, c)
 
 }
