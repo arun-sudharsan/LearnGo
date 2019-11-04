6ada761f3a7d6a2fded749537c8b98e36e44506c
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 12:08:05 +0530

    Declared not used, name shadowing

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -12,6 +12,10 @@ var (
 )
 
 func main() {
+	//local scope takes preference. Name Shadowing.
+	var name = "UknowWho"
+	//j declared but not used error
+	//j:=10
 	fmt.Printf("%v, %T\n", global, global)
 	fmt.Printf("%v, %T\n", name, name)
 	fmt.Printf("%v, %T\n", age, age)
