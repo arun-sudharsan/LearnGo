0a0dc859f6e9995c92c30be4a34d97496e31de9f
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 11:55:48 +0530

    Added different types of variable inits

--- src/github.com/arunm619/helloworld/Main.go
+++ src/github.com/arunm619/helloworld/Main.go
@@ -7,8 +7,13 @@ func callME() {
 }
 
 func main() {
-	var i int
-	i = 42
-	i = 27
-	fmt.Println(i)
+	var i int //declare but no init at this time
+	i = 1     //initialisation happens after some time.
+	fmt.Printf("%v %T\n", i, i)
+
+	var j = 10. //start with some value
+	fmt.Printf("%v %T\n", j, j)
+	//either int or float64
+	k := 100.
+	fmt.Printf("%v %T\n", k, k)
 }
